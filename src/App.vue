2c7f7b3228aeadc5e671683767db5710cb936e4b
Yanuar Gandhira <yanuar.ghandira@gmail.com>
2019-06-15 22:47:16 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <CobaHeader/>
+    <CobaBody />
+  </div>
+</template>
+
+<script>
+import CobaHeader from './components/CobaHeader.vue'
+import CobaBody from './components/CobaBody.vue'
+
+export default {
+  name: 'app',
+  components: {
+    CobaHeader,
+    CobaBody
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/CobaHeader.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+  <b-navbar toggleable="sm" type="light" variant="light">
+    <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
+
+    <b-navbar-brand>Diecastkus</b-navbar-brand>
+  </b-navbar>
+</div>
+</template>
